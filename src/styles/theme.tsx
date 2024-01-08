69b12f91d2cf15e6aba8ed4ca14d70ad8e02f747
hm_yang <yyhannmingg@gmail.com>
2024-01-08 23:27:29 +0800

    more theme stuff

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
-import { lightBlue, orange } from '@mui/material/colors';
+import { cyan, lightBlue, orange } from '@mui/material/colors';
 
 export const DefaultTheme = createTheme({
     palette: {
         mode: 'light',
-        primary: lightBlue,
+        primary: cyan,
         secondary: orange,
     },
     typography: {
